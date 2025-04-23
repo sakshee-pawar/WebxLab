ec8668f8ceab0116706c2fb27e3beeab6bfd3a54
sakshee-pawar <sakshee@acpce.ac.in>
2025-04-23 22:14:44 +0530

    Update index.js

--- EXP_10/index.js
+++ EXP_10/index.js
@@ -22,10 +22,37 @@ const studentSchema = new mongoose.Schema({
 const Student = mongoose.model("Student",studentSchema);
 
 //adding a single documnet
-const st1 = new Student({
-    name:"Harry",
-    age:14,
-    email:"Potter@gmail.com"
+// const st1 = new Student({
+//     name:"Harry",
+//     age:14,
+//     email:"Potter@gmail.com"
+// });
+
+Student.insertMany([
+    {
+        name:"Hermione",
+        age:14,
+        email:"Granger@yahoo.in"
+    },
+    {
+        name:"Ronald",
+        age:14,
+        email:"Weasley@hotmail.com"
+    },
+    {
+        name:"Draco",
+        age:14,
+        email:"Malfoy@gmail.com"
+    },
+    {
+        name:"Ginny",
+        age:13,
+        email:"gWeasley@gmail.com"
+    }
+]).then(res=>{
+    console.log(`Saved successfully !`);
+}).catch(err=>{
+    console.log(`something went wrong ! ${err}`)
 });
 
-st1.save();
+// st1.save();
